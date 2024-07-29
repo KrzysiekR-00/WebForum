fc51f0611caae970a7427ad0df47a8a42d6cc305
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-07-29 07:34:03 +0200

    Add topic and post model classes

---
+++ WebForum/WebForum/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace WebForum.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = null!;
+    public DateTime DateTime { get; set; }
+    public string Author { get; set; } = null!;
+
+    public int TopicId { get; set; }
+    public Topic Topic { get; set; } = null!;
+}
---
+++ WebForum/WebForum/Models/Topic.cs
@@ -0,0 +1,9 @@
+﻿namespace WebForum.Models;
+
+public class Topic
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+
+    public ICollection<Post> Posts { get; set; } = null!;
+}
