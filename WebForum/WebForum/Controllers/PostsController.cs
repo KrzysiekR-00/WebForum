8a8c2dcf7226ba7f0e68fd364896dc628de37896
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-08-02 11:18:27 +0200

    Remove unused topics and posts views and actions

--- WebForum/WebForum/Controllers/PostsController.cs
+++ WebForum/WebForum/Controllers/PostsController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebForum.Data;
 using WebForum.Models;
@@ -18,13 +17,6 @@ namespace WebForum.Controllers
         {
             _context = context;
             _userManager = userManager;
-        }
-
-        // GET: Posts
-        public async Task<IActionResult> Index()
-        {
-            var webForumContext = _context.Posts.Include(p => p.Topic);
-            return View(await webForumContext.ToListAsync());
         }
 
         // GET: Posts/Details/5
@@ -94,98 +86,6 @@ namespace WebForum.Controllers
             }
             //ViewData["TopicId"] = new SelectList(_context.Topics, "Id", "Id", post.TopicId);
             return View(newPostViewModel);
-        }
-
-        // GET: Posts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var post = await _context.Posts.FindAsync(id);
-            if (post == null)
-            {
-                return NotFound();
-            }
-            ViewData["TopicId"] = new SelectList(_context.Topics, "Id", "Id", post.TopicId);
-            return View(post);
-        }
-
-        // POST: Posts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Content,DateTime,Author,TopicId")] Post post)
-        {
-            if (id != post.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(post);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PostExists(post.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["TopicId"] = new SelectList(_context.Topics, "Id", "Id", post.TopicId);
-            return View(post);
-        }
-
-        // GET: Posts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var post = await _context.Posts
-                .Include(p => p.Topic)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (post == null)
-            {
-                return NotFound();
-            }
-
-            return View(post);
-        }
-
-        // POST: Posts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var post = await _context.Posts.FindAsync(id);
-            if (post != null)
-            {
-                _context.Posts.Remove(post);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool PostExists(int id)
-        {
-            return _context.Posts.Any(e => e.Id == id);
         }
     }
 }
