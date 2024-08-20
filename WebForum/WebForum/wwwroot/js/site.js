1bd471232ec7fc1079e5adadd39de88f4b4a59a0
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-08-20 14:00:15 +0200

    Move dark mode code to site.js file

--- WebForum/WebForum/wwwroot/js/site.js
+++ WebForum/WebForum/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleDarkMode() {
+    $('.text-light, .text-dark').toggleClass('text-light text-dark');
+    $('.navbar-light, .navbar-dark').toggleClass('navbar-light navbar-dark');
+    $('.bg-white, .bg-dark').toggleClass('bg-white bg-dark');
+    $('.table-white, .table-dark').toggleClass('table-white table-dark');
+}
